20bc0db4196c49226f80c5d41bba5f5d900f078d
Idder Bouram <ibouram@e3r1p12.1337.ma>
2024-08-14 14:37:04 +0100

    [ADD] add 2 functios init_forks + init_philos

--- philo/init.c
+++ philo/init.c
@@ -6,20 +6,63 @@
 /*   By: ibouram <ibouram@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 09:54:13 by ibouram           #+#    #+#             */
-/*   Updated: 2024/08/09 11:40:14 by ibouram          ###   ########.fr       */
+/*   Updated: 2024/08/14 14:33:37 by ibouram          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_arg(char **str, t_philo *philo)
+void	init_arg(char **str, t_init *data)
 {
-	philo->nb_philo = *str[1];
-	philo->time_to_die = *str[2];
-	philo->time_to_eat = *str[3];
-	philo->time_to_sleep = *str[4];
+	data->nb_philo = *str[1];
+	data->time_to_die = *str[2];
+	data->time_to_eat = *str[3];
+	data->time_to_sleep = *str[4];
 	if (str[5])
-		philo->nb_philo = *str[5];
-	// else
-	// 	philo->nb_philo = -1;
+		data->nb_time_eat = *str[5];
+	else
+		data->nb_time_eat = -1;
+}
+
+void	init_forks(t_init *data)
+{
+	int	i;
+
+	i = 0;
+	data->forks = malloc (sizeof(pthread_mutex_t) * (data->nb_philo));
+	if (!data->forks)
+		return ;
+	while (i < data->nb_philo)
+	{
+		pthread_mutex_init(&data->forks[i], NULL);
+		i++;
+	}
+}
+
+void	init_philos(t_init *data)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	data->philos = malloc (sizeof(pthread_mutex_t) * (data->nb_philo));
+	if (!data->philos)
+		return ;
+	while (i < data->nb_philo)
+	{
+		data->philos[i].id = i + 1;
+		data->philos[i].nb_meals = 0;
+		data->philos[i].start_time = get_time();
+		data->philos[i].last_meal = get_time();
+		data->philos[i].left_f = &data->forks[i];
+		data->philos[i].right_f = &data->forks[i + 1 % data->nb_philo];
+		i++;
+	}
+	j = 0;
+	while (j < data->nb_philo)
+	{
+		// pthread_mutex_init(&data->philos[i], NULL);
+		// pthread_create(&data->philos[i].thread, NULL, routine, NULL);
+		j++;
+	}
 }
