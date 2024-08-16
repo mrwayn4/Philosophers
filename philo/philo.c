c45d9abce483a8a247a8fb588e417eab6accfed2
Idder Bouram <ibouram@e3r2p7.1337.ma>
2024-08-16 18:17:01 +0100

    [ADD] add time funct + [EDIT] in main pth_creat && pth_detach

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: ibouram <ibouram@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 18:47:29 by ibouram           #+#    #+#             */
-/*   Updated: 2024/08/13 10:07:18 by ibouram          ###   ########.fr       */
+/*   Updated: 2024/08/16 18:10:05 by ibouram          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,33 @@ int	check_input(char **s)
 	return (0);
 }
 
+int	ft_usleep(size_t milliseconds)
+{
+	size_t	start;
+
+	start = get_time();
+	while ((get_time() - start) < milliseconds)
+		usleep(500);
+	return (0);
+}
+
+size_t	get_time(void)
+{
+	struct timeval	time;
+	size_t	time_s;
+	size_t	time_mcs;
+
+	gettimeofday(&time, NULL);
+	time_s = time.tv_sec * 1000;
+	time_mcs = time.tv_usec / 1000;
+	return (time_s + time_mcs);
+}
+
 int	main(int ac, char **av)
 {
 	t_init	philo;
+	int		i;
+	int		j;
 
 	if (ac != 5 && ac != 6)
 	{
@@ -42,8 +66,13 @@ int	main(int ac, char **av)
 	}
 	if (check_input(av) == 1)
 		return (1);
-	init_arg(av, &philo);
-	init_forks(&philo);
-	init_philos(&philo);
+	init_data(av, &philo);
+	(1) && (i = -1 , j = -1);
+	while (++i < philo.nb_philo)
+		pthread_create(&philo.philos[i].thread, NULL, &routine, &philo.philos[i]);
+	while (++j < philo.nb_philo)
+		pthread_detach(philo.philos[j].thread);
+	// monitor(&philo);
+	// destroy(&philo);
 	return (0);
 }
